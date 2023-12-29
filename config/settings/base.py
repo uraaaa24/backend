a45f786264943c29004b48b8a750700c4df7f350
Yuto Urashima <yuto.urashima24@gmail.com>
2023-12-29 10:00:02 +0000

    設定を追加

--- config/settings/base.py
+++ config/settings/base.py
@@ -121,3 +121,19 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandlers"
+            }
+        },
+        "loggers": {
+            "django.db.backends": {
+                "level": "DEBUG",
+                "handlers": ["console"]
+            }
+        }
+}
