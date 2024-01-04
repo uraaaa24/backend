5c4cb070c01eb6e6505037f0bd1199c60144c1b4
Yuto Urashima <yuto.urashima24@gmail.com>
2024-01-04 07:49:12 +0000

    更新処理を作成

--- api/inventory/urls.py
+++ api/inventory/urls.py
@@ -4,6 +4,7 @@ from api.inventory import views
 
 urlpatterns = [
     path("products/", views.ProductView.as_view()),
+    path("products/<int:id>/", views.ProductView.as_view()),
     path("purchases/", views.PurchaseView.as_view()),
     path("sales/", views.SalesView.as_view())
 ]
--- api/inventory/views.py
+++ api/inventory/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -12,15 +13,28 @@ class ProductView(APIView):
   商品操作に関する関数
   """
   
-  def get_product_data(self, request, format=None):
+  def get_object(self, pk):
     """
-    商品の一覧を取得する
+    商品操作に関する関数で共通で使用する商品取得関数
     """
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
+    try:
+      return Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+      raise NotFound
+
+  def get(self, request, id=None, format=None):
+    """
+    商品の一覧もしくは一意の商品を取得する
+    """
+    if id is None:
+      queryset = Product.objects.all()
+      serializer = ProductSerializer(queryset, many=True)
+    else:
+      product = self.get_object(id)
+      serializer = ProductSerializer(product)
     return Response(serializer.data, status.HTTP_200_OK)
 
-  def post_product_data(self, request, format=None):
+  def post(self, request, format=None):
     """
     商品を登録する
     """
@@ -28,10 +42,20 @@ class ProductView(APIView):
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data, status.HTTP_201_CREATED)
+  
+  def put(self, request, id, format=None):
+    """
+    商品の情報を更新する
+    """
+    product = self.get_object(id)
+    serializer = ProductSerializer(instance=product, data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status.HTTP_200_OK)
 
 
 class PurchaseView(APIView):
-  def post_purchase_data(self, request, format=None):
+  def post(self, request, format=None):
     """
     仕入れ情報を登録する
     """
@@ -42,7 +66,7 @@ class PurchaseView(APIView):
 
 
 class SalesView(APIView):
-  def post_sales_data(self, request, format=None):
+  def post(self, request, format=None):
     """
     売上情報を登録する
     """
