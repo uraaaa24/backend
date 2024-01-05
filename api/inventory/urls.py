566b5d8e49b3fadf9aa0c27936ee7f3e5274c347
Yuto Urashima <yuto.urashima24@gmail.com>
2024-01-05 07:30:10 +0000

    JWTの作成

--- api/inventory/urls.py
+++ api/inventory/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from api.inventory import views
 
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     path("products/", views.ProductView.as_view()),
     path("products/<int:id>/", views.ProductView.as_view()),
-    path(
-        "products/model/", views.ProductView.as_view({"get": "list", "post": "create"})
-    ),
     path("purchases/", views.PurchaseView.as_view()),
     path("sales/", views.SalesView.as_view()),
-    path("inventory/<int:id>/", views.InventoryView.as_view()),
+    path("inventories/<int:id>/", views.InventoryView.as_view()),
 ]
--- config/settings/base.py
+++ config/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from timedelta import datetime
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,6 +85,12 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=30),
+    "UPDATE_LAST_LOGIN": True,
 }
 
 # Database
