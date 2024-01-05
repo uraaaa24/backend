fe7154c66c59eeb5111a99d0127a70c80b8eacc5
Yuto Urashima <yuto.urashima24@gmail.com>
2024-01-05 06:18:02 +0000

    urlにinventory追加

--- api/inventory/urls.py
+++ api/inventory/urls.py
@@ -5,6 +5,10 @@ from api.inventory import views
 urlpatterns = [
     path("products/", views.ProductView.as_view()),
     path("products/<int:id>/", views.ProductView.as_view()),
+    path(
+        "products/model/", views.ProductView.as_view({"get": "list", "post": "create"})
+    ),
     path("purchases/", views.PurchaseView.as_view()),
     path("sales/", views.SalesView.as_view()),
+    path("inventory/<int:id>/", views.InventoryView.as_view()),
 ]
